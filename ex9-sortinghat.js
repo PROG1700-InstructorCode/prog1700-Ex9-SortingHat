6a78e8159bacbc55bb2d52bee257c054cb454989
hamletnscc <113032060+hamletnscc@users.noreply.github.com>
2023-02-04 16:25:52 -0400

    Create ex9-sortinghat.js

---
+++ ex9-sortinghat.js
@@ -0,0 +1,13 @@
+"use strict";
+var readlineSync = require("readline-sync");
+// DO NOT EDIT: The main function to house our program code 
+
+function main()
+{
+}
+
+// DO NOT EDIT: Trigger our main function to launch the program
+if (require.main === module)
+{
+    main();
+}
